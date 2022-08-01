94f5acfe55bf4a669c2b80ee094c6dcb55853377
zkeq <admin@icodeq.com>
2022-08-01 18:15:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log("hi")
 import x from './x.js'
 console.log(x)
+console.log("build")
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "production", // "production" | "development" | "none"
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      title: 'Webpack Demo',
+      template: 'src/assets/index.html'
+    }
+  )],
   output: {
     path: path.resolve(__dirname + "/dist"),
     filename: "[name].[contenthash].js"
-}
+} 
 };
