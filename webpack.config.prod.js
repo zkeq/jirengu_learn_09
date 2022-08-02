09ba7bd7d154d5be1749f703164df2141d358b07
zkeq <admin@icodeq.com>
2022-08-02 18:58:16 +0800

    官网方法

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const base = require('./webpack.config.base.js');
 
 module.exports = {
@@ -9,27 +9,26 @@ module.exports = {
     rules: [
       ...base.module.rules,
       {
-        test: /\.css$/i,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            // you can specify a publicPath here
-            // by default it uses publicPath in webpackOptions.output
-            publicPath: "../",
-          },
-        }, 
-        "css-loader"],
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
     ],
   },
   plugins: [
     ...base.plugins,
     new MiniCssExtractPlugin(
-    {
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css",
-      ignoreOrder: false,
-    }),
-    new OptimizeCSSAssetsPlugin({})
+      {
+        filename: "[name].[contenthash].css",
+        chunkFilename: "[id].[contenthash].css",
+        ignoreOrder: false,
+      }
+    )
 ],
 };
