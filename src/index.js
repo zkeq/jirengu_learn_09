d638b5a11ddb6fb974cb18939eaedac139d2ff47
zkeq <admin@icodeq.com>
2022-08-02 16:02:19 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -11,3 +11,13 @@ console.log(x)
 div.innerHTML = `
     <img src="${png}">
 `
+
+const button = document.createElement('button');
+button.innerText = '懒加载';
+button.onclick = () => {
+    import('./lazy').then(module => {
+        console.log(module)
+        module.default()
+    })
+}
+div.appendChild(button);
